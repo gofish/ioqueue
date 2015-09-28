9dce18b6479cee5495b8941b4bb365af5f28c9aa
Jeremy R. Fishman <jeremy@clover.com>
2015-09-28 11:46:20 -0700

    fix relative depends for subdir builds

--- perf/Rules.mk
+++ perf/Rules.mk
@@ -3,11 +3,11 @@ LDFLAGS := -pthread
 TGTS += bench
 SRCS += bench.cc
 
-$(call depends_ext,bench,libioqueue.a)
+$(call depends,bench,../libioqueue.a)
 $(call depends_ext,bench,-laio)
 $(call depends_ext,bench,-lrt)
 
 TGTS += benchmt
 SRCS += benchmt.cc
 
-$(call depends_ext,benchmt,libioqueuemt.a)
+$(call depends,benchmt,../libioqueuemt.a)
--- test/Rules.mk
+++ test/Rules.mk
@@ -4,7 +4,7 @@ LDLIBS  := -lgtest -lgtest_main
 TGTS += ioqueue.t
 SRCS += ioqueue.t.cc
 
-$(call depends_ext,ioqueue.t,libioqueue.a)
+$(call depends,ioqueue.t,../libioqueue.a)
 $(call depends_ext,ioqueue.t,-laio)
 $(call depends_ext,ioqueue.t,-lrt)
 $(call test,ioqueue.t)
@@ -12,5 +12,5 @@ $(call test,ioqueue.t)
 TGTS += ioqueuemt.t
 SRCS += ioqueuemt.t.cc
 
-$(call depends_ext,ioqueuemt.t,libioqueuemt.a)
+$(call depends,ioqueuemt.t,../libioqueuemt.a)
 $(call test,ioqueuemt.t)
