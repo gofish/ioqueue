ef03da19b25e4072b7755b5e856141db26846663
Jeremy R. Fishman <jeremy@clover.com>
2015-10-03 11:34:42 -0700

    fixup licenses

--- ioqueue.c
+++ ioqueue.c
@@ -1,3 +1,22 @@
+
+// ioqueue.c - Linux Kernel AIO implementation of the ioqueue API
+//
+// Copyright (C) 2015  Jeremy R. Fishman
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// and Lesser General Public License along with this program. If not,
+// see <http://www.gnu.org/licenses/>.
+
 #include <errno.h>
 #include <sys/time.h>
 #include <sys/types.h>
